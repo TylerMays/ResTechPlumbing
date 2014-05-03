a65e8fee297dcedc270bdf7a1778b15d88f65db6
Patrick Armitage <armitage.patrick@gmail.com>
2014-05-02 22:48:21 -0700

    Intialize HomePages Controller

---
+++ app/controllers/home_pages_controller.rb
@@ -0,0 +1,22 @@
+class HomePagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def residential
+  end
+
+  def commercial
+  end
+
+  def products
+  end
+
+  def plumbing
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,17 @@
 ResTechPlumbing::Application.routes.draw do
+  get 'home_pages/home'
+
+  get 'home_pages/about'
+
+  get 'home_pages/residential'
+
+  get 'home_pages/commercial'
+
+  get 'home_pages/products'
+
+  get 'home_pages/plumbing'
+
+  get 'home_pages/contact'
+
+  root to: 'home_pages#home'
 end
